049fbffb9c4d6f55655c84c9696d87de85ed4928
silverwebbuzz <silverwebbuzz@gmail.com>
2025-10-01 21:42:52 +0530

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CarbonTracker - Professional Carbon Emissions Tracking</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 <body class="bg-gray-50">
     <section class="bg-gradient-to-br from-eco-green to-eco-green-dark text-white py-24">
