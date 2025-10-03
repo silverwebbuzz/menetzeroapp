87b27f41f59ecbfd4f654394ce0e9bb1aa91c20d
silverwebbuzz <silverwebbuzz@gmail.com>
2025-10-03 17:58:08 +0530

    changes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'role' => 'user',
+            'role' => 'company_user',
             'is_active' => true,
         ]);
 
