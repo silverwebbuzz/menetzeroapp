b6af1447b151969ea9516b3451bd0c998c1016a2
silverwebbuzz <silverwebbuzz@gmail.com>
2025-12-10 18:02:41 +0530

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -376,7 +376,7 @@
                             Reports
                         </a>
                         
-                        <a href="{{ route('profile.index') }}" class="nav-link {{ request()->routeIs('profile.*') ? 'active' : '' }}">
+                        <a href="{{ route('client.profile') }}" class="nav-link {{ request()->routeIs('client.profile') || request()->routeIs('profile.*') ? 'active' : '' }}">
                             <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
                             </svg>
