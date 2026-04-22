ac08782d40f3d395b9590bd9adda492c9db60226
silverwebbuzz <silverwebbuzz@gmail.com>
2026-04-22 17:37:16 +0530

    changes by bhavik

--- database/migrations/2024_01_01_000001_create_admins_table.php
+++ database/migrations/2024_01_01_000001_create_admins_table.php
@@ -11,6 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (Schema::hasTable('admins')) {
+            return;
+        }
+
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->string('name');
