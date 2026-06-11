2c3bb7c3122f8b828f27e6b81ecaf013ebfd4c1c
silverwebbuzz <silverwebbuzz@gmail.com>
2026-06-11 18:32:48 +0530

    Changes in fonts size and weight

--- resources/views/consultant/layouts/app.blade.php
+++ resources/views/consultant/layouts/app.blade.php
@@ -48,7 +48,7 @@
     </script>
 
     <link rel="stylesheet" href="{{ asset('css/app-shell.css') }}?v=20260612">
-    <link rel="stylesheet" href="{{ asset('css/consultant-shell.css') }}?v=20260613">
+    <link rel="stylesheet" href="{{ asset('css/consultant-shell.css') }}?v=20260615">
 
     @stack('styles')
 
